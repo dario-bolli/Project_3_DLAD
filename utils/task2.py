5017d21f5c480a5aeb0f6a281b7c69b3f006bb91
Nicolashoi <nhoischen@student.ethz.ch>
2021-06-01 22:47:48 +0200

    small changes

--- utils/task2.py
+++ utils/task2.py
@@ -1,10 +1,12 @@
 import numpy as np
 import random
-
+import time
+#import numba as nb
 
 #==================================#
 # Rotation Matrix
 #==================================#
+
 def z_rotation(theta):
     """
     Rotation about the -z-axis. 
@@ -18,13 +20,13 @@ def z_rotation(theta):
                      [0, 0, 1]])              
     return Rot
 
-def label2corners(label):
+def label2corners(label, config):
     '''
     Task 1
     input
         label (N,7) 3D bounding box with (x,y,z,h,w,l,ry)
     output
-        corners (8,3) corner coordinates in the rectified reference frame
+        corners (N,8,3) corner coordinates in the rectified reference frame
 
         (8, 3) array of vertices for the 3D box in
         following order:
@@ -37,30 +39,40 @@ def label2corners(label):
           6 -------- 7
 
     '''
-    # extract the dimensions of the box
-    height = label[3]
-    width = label[4]
-    length = label[5]
-    #center coordinates
-    x =  label[0]
-    y =  label[1]
-    z =  label[2]
+    corners = np.zeros((label.shape[0],8,3))
+    start = time.time()
+    for i in range(label.shape[0]):
+        # extract the dimensions of the box
+        height = label[i,3] + config['delta']
+        width = label[i,4] + config['delta']
+        length = label[i,5] + config['delta']
+        #center coordinates
+        x =  label[i,0]
+        y =  label[i,1]
+        z =  label[i,2]
 
-    # Corners location 3D in velodyne frame
-    corners_x = [width/2,- width/2, -width/2, width/2, width/2, -width/2, -width/2, width/2]
-    corners_y = [length/2, length/2, -length/2, -length/2, length/2, length/2, -length/2, -length/2]
-    corners_z = [height, height, height, height, 0, 0, 0, 0]
-    # Dimensions of box_dim: 3 x 8 i.e. rows are (x,y,z) and columns are the corners
-    Rot = z_rotation(label[6])
-    box = Rot@np.vstack([corners_x, corners_y, corners_z])
-    box[0,:] += x
-    box[1,:] += y
-    box[2,:] += z
-    # Dimensions of box_dim: 3 x 8 i.e. rows are (x,y,z) and columns are the corners
-    corners = np.transpose(box) 
-    #print("corners",corners)
-    return corners
-    
+        # Corners location 3D in velodyne frame
+        '''
+        corners_x = [x+width/2, x-width/2, x-width/2, x+width/2,x+width/2, x-width/2, x-width/2, x+width/2]
+        corners_y = [y+length/2, y+length/2, y-length/2, y-length/2, y+length/2, y+length/2, y-length/2, y-length/2]
+        corners_z = [z+height, z+height, z+height, z+height, z, z, z, z]
+        '''
+        corners_x = [width/2,- width/2, -width/2, width/2, width/2, -width/2, -width/2, width/2]
+        corners_y = [length/2, length/2, -length/2, -length/2, length/2, length/2, -length/2, -length/2]
+        corners_z = [height, height, height, height, 0, 0, 0, 0]
+        #Rotation around z-axis
+        Rot = z_rotation(label[i,6])
+
+        box_dim = Rot@np.vstack([corners_x, corners_y, corners_z])
+        box_dim[0,:] += x
+        box_dim[1,:] += y
+        box_dim[2,:] += z
+        # Dimensions of box_dim: 3 x 8 i.e. rows are (x,y,z) and columns are the corners
+        corners[i,:,:] = np.transpose(box_dim) 
+    end = time.time()
+    print("time in the label2corners function is " , end-start)
+    return corners 
+
 
 def roi_pool(pred, xyz, feat, config):
     '''
@@ -90,35 +102,72 @@ def roi_pool(pred, xyz, feat, config):
         config['max_points'] number of points in the final sampled ROI
     '''
     #pooled_xyz = np.zeros((1,512,3))
+    # IMPORTANT N = 100 for pred (boundig boxes) but N  =16384 for xyz (point cloud)
     pooled_xyz = []
     valid_pred = []
     pooled_feat = []
     max_pointsROI = config['max_points']
     #https://stackoverflow.com/questions/21037241/how-to-determine-a-point-is-inside-or-outside-a-cube
-    for i in range(pred.shape[0]):
-        pred[i,3:5]= pred[i,3:5] + config['delta']
-        corners = label2corners(pred[i,:]) # shape 8 x 3
-        u = (corners[5,:] - corners[6,:])
+    print("N is equal", pred.shape[0])
+    corners = label2corners(pred, config)
+    print("corners shape", corners.shape[0])
+    """
+    u =  corners[:,5,:] - corners[:,6,:]
+    norm_u = np.linalg.norm(u)
+    u = u/norm_u
+    v = (corners[:,7,:] - corners[:,6,:])
+    norm_v = np.linalg.norm(v)
+    v = v/norm_v
+    w = (corners[:,2,:] - corners[:,6,:])
+    norm_w = np.linalg.norm(w)
+    w = w/norm_w
+    """
+    
+    for i in range(corners.shape[0]):
+        
+        u = np.array(corners[i,5,:] - corners[i,6,:])
         norm_u = np.linalg.norm(u)
         u = u/norm_u
-        v = (corners[7,:] - corners[6,:])
+        v = np.array(corners[i,7,:] - corners[i,6,:])
         norm_v = np.linalg.norm(v)
         v = v/norm_v
-        w = (corners[2,:] - corners[6,:])
+        w = np.array(corners[i,2,:] - corners[i,6,:])
         norm_w = np.linalg.norm(w)
         w = w/norm_w
         #box_center = (corners[6,:] + corners[0,:])/2.0
-        boxPoints = np.zeros((max_pointsROI,3))
-        boxFeat= np.zeros((max_pointsROI,feat.shape[1]))
+        
         #center2point = xyz - box_center
-        center2point = xyz - corners[6,:]
+        center2point = np.array(xyz - corners[i,6,:])
+        
+        start = time.time()
         cond1 = set(np.where((np.absolute(np.dot(center2point, u))) <= norm_u )[0])
         cond2 = set(np.where((np.absolute(np.dot(center2point, v))) <= norm_v )[0])
         cond3 = set(np.where((np.absolute(np.dot(center2point, w))) <= norm_w )[0])
-        
         xyz_indic = list(cond1.intersection( cond2, cond3 ))
+
+        end = time.time()
+        #print("Find points execution time for conditions is ", end - start)
+        boxPoints = np.zeros((max_pointsROI,3))
+        boxFeat= np.zeros((max_pointsROI,feat.shape[1]))
+       
+        
+        """
+        #xyz_indic = []
+        #print(center2point.shape)
+        #print(np.dot(center2point, u).shape)
+        start = time.time()
+        xyz_indic = []
+        for k in range(xyz.shape[0]):
+            if abs(np.dot(center2point[k,:], u)) <= norm_u and abs(np.dot(center2point[k,:], v)) and abs(np.dot(center2point[k,:], w)) <= norm_w: 
+                #xyz_indic.append(k)
+                xyz_indic.append(k)
+        end = time.time()
+        print("Find points execution time is ", end - start)
+        
+        """
         boxPoints = xyz[xyz_indic,:]
         boxFeat= feat[xyz_indic, :]
+        
         # print(len(xyz_indic))
         if 1 < len(xyz_indic) < max_pointsROI:
             nb_fill = max_pointsROI-len(xyz_indic)
@@ -139,13 +188,15 @@ def roi_pool(pred, xyz, feat, config):
         pooled_xyz.append(boxPoints)
         pooled_feat.append(boxFeat)
         valid_pred.append(pred[i,:])
-
+    
     pooled_xyz = np.array(pooled_xyz)
     pooled_feat = np.array(pooled_feat)
     valid_pred = np.array(valid_pred)
     # print("valid pred shape", valid_pred.shape)
     # print("pooled_xyz shape", pooled_xyz.shape)
     # print("pooled_feat shape", pooled_feat.shape)
+    
+   
     return valid_pred, pooled_xyz, pooled_feat
 
 
