923bafb46ddee33fbbd7277e7c59f6903991f33d
Nicolashoi <nhoischen@student.ethz.ch>
2021-06-20 22:17:19 +0200

    task2 around 95ms

--- utils/task2.py
+++ utils/task2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 import time
-#import numba as nb
+import numba as nb
 
 #==================================#
 # Rotation Matrix
@@ -74,6 +74,82 @@ def label2corners(label, config):
     return corners 
 
 
+#@nb.njit(cache = True, nogil = True)
+def indexInBox(directions, center2point, norm_u, norm_v, norm_w):
+    #projection = np.absolute(np.matmul(center2point,directions))
+        #print(projection.shape)
+    
+    projection = np.absolute(center2point@directions)   
+    #start = time.time()
+    cond1 = np.flatnonzero(projection[:,2] <= norm_w )
+    end = time.time()
+    #print("cond1 time", end -start)
+    cond2 = np.flatnonzero(projection[cond1,0] <= norm_u )
+    xyz_indic = np.flatnonzero(projection[cond2,1] <= norm_v )
+
+    return xyz_indic
+
+# @nb.njit
+# def loopBoxes(corners,xyz, pred, feat, max_pointsROI):
+#     pooled_xyz = []
+#     valid_pred = []
+#     pooled_feat = []
+#     # u =  corners[:,5,:] - corners[:,6,:]
+#     # norm_u = np.linalg.norm(u,axis=1)
+#     # u = u/norm_u[:,None]
+#     # v = (corners[:,7,:] - corners[:,6,:])
+#     # norm_v = np.linalg.norm(v,axis = 1)
+#     # v = v/norm_v[:,None]
+#     # w = (corners[:,2,:] - corners[:,6,:])
+#     # norm_w = np.linalg.norm(w,axis=1)
+#     # w = w/norm_w[:,None]
+   
+#     for i in range(corners.shape[0]):
+#         u = corners[i,5,:] - corners[i,6,:]
+#         norm_u = np.linalg.norm(u)
+#         u = u/norm_u
+#         v = corners[i,7,:] - corners[i,6,:]
+#         norm_v = np.linalg.norm(v)
+#         v = v/norm_v
+#         w = corners[i,2,:] - corners[i,6,:]
+#         norm_w = np.linalg.norm(w)
+
+#         center2point = xyz - corners[i,6,:]
+
+#         directions = np.stack((u, v, w), axis = 1)
+    
+#         #start = time.time()
+       
+#         # projection = np.absolute(center2point@directions)   
+#         # cond1 = np.flatnonzero(projection[:,0] <= norm_u )
+#         # cond2 = np.flatnonzero(projection[cond1,1] <= norm_v )
+#         # xyz_indic = np.flatnonzero(projection[cond2,2] <= norm_w )
+#         xyz_indic = indexInBox(directions, center2point, norm_u, norm_v, norm_w)
+#        # end = time.time()
+        
+#         # boxPoints = np.zeros((max_pointsROI,3))
+#         # boxFeat= np.zeros((max_pointsROI,feat.shape[1]))
+       
+      
+#         if 1 < len(xyz_indic) < max_pointsROI:
+#             idx = np.random.choice(xyz_indic, size=max_pointsROI, replace=True)
+#             boxPoints = xyz[idx,:]
+#             boxFeat = feat[idx,:]
+    
+
+#         elif len(xyz_indic) > max_pointsROI:
+#             idx = np.random.choice(xyz_indic, size=max_pointsROI, replace=False)
+#             boxPoints = xyz[idx,:]
+#             boxFeat = feat[idx,:]
+        
+#         else: # just one point in this box, discard
+#             continue
+#         pooled_xyz.append(boxPoints)
+#         pooled_feat.append(boxFeat)
+#         valid_pred.append(pred[i,:])
+#     return pooled_xyz, pooled_feat, valid_pred
+
+
 def roi_pool(pred, xyz, feat, config):
     '''
     Task 2
@@ -108,116 +184,59 @@ def roi_pool(pred, xyz, feat, config):
     pooled_feat = []
     max_pointsROI = config['max_points']
     #https://stackoverflow.com/questions/21037241/how-to-determine-a-point-is-inside-or-outside-a-cube
-    print("N is equal", pred.shape[0])
+    #print("N is equal", pred.shape[0])
     corners = label2corners(pred, config)
-    print("corners shape", corners.shape[0])
-    """
+    #print("corners shape", corners.shape[0])
+    #vectores of shape (nb boxes = 100, 3)
+    #pooled_xyz, pooled_feat, valid_pred = loopBoxes(corners,xyz, pred, feat, max_pointsROI)
+    ##### Takes 1.2 ms #######
     u =  corners[:,5,:] - corners[:,6,:]
-    norm_u = np.linalg.norm(u)
-    u = u/norm_u
+    norm_u = np.linalg.norm(u,axis=1)
+    u = u/norm_u[:,None]
     v = (corners[:,7,:] - corners[:,6,:])
-    norm_v = np.linalg.norm(v)
-    v = v/norm_v
+    norm_v = np.linalg.norm(v,axis = 1)
+    v = v/norm_v[:,None]
     w = (corners[:,2,:] - corners[:,6,:])
-    norm_w = np.linalg.norm(w)
-    w = w/norm_w
-    """
+    norm_w = np.linalg.norm(w,axis=1)
+    w = w/norm_w[:,None]
+    #box_center = (corners[:,6,:] + corners[:,0,:])/2.0
+    #####################
     
     for i in range(corners.shape[0]):
         
-        u = np.array(corners[i,5,:] - corners[i,6,:])
-        norm_u = np.linalg.norm(u)
-        u = u/norm_u
-        v = np.array(corners[i,7,:] - corners[i,6,:])
-        norm_v = np.linalg.norm(v)
-        v = v/norm_v
-        w = np.array(corners[i,2,:] - corners[i,6,:])
-        norm_w = np.linalg.norm(w)
-        w = w/norm_w
-        #box_center = (corners[6,:] + corners[0,:])/2.0
-        
-        #center2point = xyz - box_center
+        #center2point = xyz - box_center[i,:]
         center2point = np.array(xyz - corners[i,6,:])
-        
-        start = time.time()
-        cond1 = set(np.where((np.absolute(np.dot(center2point, u))) <= norm_u )[0])
-        cond2 = set(np.where((np.absolute(np.dot(center2point, v))) <= norm_v )[0])
-        cond3 = set(np.where((np.absolute(np.dot(center2point, w))) <= norm_w )[0])
-        xyz_indic = list(cond1.intersection( cond2, cond3 ))
-
-        end = time.time()
+        directions = np.stack((u[i,:], v[i,:], w[i,:]), axis = 1)
+        #start = time.time()
+        xyz_indic = indexInBox(directions, center2point, norm_u[i], norm_v[i], norm_w[i])
+        #end = time.time()
         #print("Find points execution time for conditions is ", end - start)
-        boxPoints = np.zeros((max_pointsROI,3))
-        boxFeat= np.zeros((max_pointsROI,feat.shape[1]))
-       
-        
-        """
-        #xyz_indic = []
-        #print(center2point.shape)
-        #print(np.dot(center2point, u).shape)
-        start = time.time()
-        xyz_indic = []
-        for k in range(xyz.shape[0]):
-            if abs(np.dot(center2point[k,:], u)) <= norm_u and abs(np.dot(center2point[k,:], v)) and abs(np.dot(center2point[k,:], w)) <= norm_w: 
-                #xyz_indic.append(k)
-                xyz_indic.append(k)
-        end = time.time()
-        print("Find points execution time is ", end - start)
-        
-        """
-        boxPoints = xyz[xyz_indic,:]
-        boxFeat= feat[xyz_indic, :]
-        
-        # print(len(xyz_indic))
+        # boxPoints = np.zeros((max_pointsROI,3))
+        # boxFeat= np.zeros((max_pointsROI,feat.shape[1]))
         if 1 < len(xyz_indic) < max_pointsROI:
-            nb_fill = max_pointsROI-len(xyz_indic)
-            fill_idx = np.random.randint(len(xyz_indic), size= nb_fill) # should we do uniform distribution ?
-            fill_points = boxPoints[fill_idx,:]
-            fill_feat = boxFeat[fill_idx,:]
-            boxPoints = np.vstack((boxPoints, fill_points))
-            boxFeat= np.vstack((boxFeat, fill_feat))
-
+            idx = np.random.choice(xyz_indic, size=max_pointsROI, replace=True)
+            boxPoints = xyz[idx,:]
+            boxFeat = feat[idx,:]
+    
         elif len(xyz_indic) > max_pointsROI:
-            nb_discard = len(xyz_indic) - max_pointsROI
-            delete_idx = random.sample(range(0,len(xyz_indic)), nb_discard)
-            boxPoints = np.delete(boxPoints, obj = delete_idx, axis=0)
-            boxFeat = np.delete(boxFeat, obj = delete_idx, axis=0)
-        
+            idx = np.random.choice(xyz_indic, size=max_pointsROI, replace=False)
+            boxPoints = xyz[idx,:]
+            boxFeat = feat[idx,:]
+          
         else: # just one point in this box, discard
             continue
+        
+        ####### around 1.5 micro seconds per iter * 100 = 0.15 ms
         pooled_xyz.append(boxPoints)
         pooled_feat.append(boxFeat)
         valid_pred.append(pred[i,:])
-    
+        ###########################
+        #xyz = np.delete(xyz, obj = xyz_indic, axis = 0)
+        
     pooled_xyz = np.array(pooled_xyz)
     pooled_feat = np.array(pooled_feat)
     valid_pred = np.array(valid_pred)
-    # print("valid pred shape", valid_pred.shape)
+    #print("valid pred shape", valid_pred.shape)
     # print("pooled_xyz shape", pooled_xyz.shape)
     # print("pooled_feat shape", pooled_feat.shape)
-    
-   
     return valid_pred, pooled_xyz, pooled_feat
-
-
-# if __name__ == '__main__':
-#     pooled_xyz = []
-#     valid_pred = []
-#     boxPoints = np.array([[1,2,3], [4,5,6], [7,8,9]])
-#     for i in range(5):
-#         pooled_xyz.append(boxPoints)
-#         valid_pred.append(np.array([1,2,3,4,5,6,7]))
-#     pooled_xyz = np.array(pooled_xyz)
-#     valid_pred = np.array(valid_pred)
-#     print(pooled_xyz.shape)
-#     print(valid_pred.shape)
-#     #print(random.sample(boxPoints,2))
-#     #print(np.random.choice(boxPoints,10))
-#     idx = np.random.randint(3, size = 2)
-#     nex_arr = boxPoints[idx,:]
-#     boxPoints = np.vstack((boxPoints, nex_arr))
-#     print(boxPoints)
-#     delete = random.sample(range(0,4), 2)
-#     print(delete)
-#     boxPoints = np.delete(boxPoints, obj = delete, axis=0)
-#     print(boxPoints)
