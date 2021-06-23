c1d4eb724d2f527056d55e8eb78f627eb900282c
Nicolashoi <nhoischen@student.ethz.ch>
2021-06-23 17:07:34 +0200

    task5 almost successfull but some rounding errors probably -> test fail

--- utils/task5.py
+++ utils/task5.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from utils.task1 import get_iou
 
+
 def nms(pred, score, threshold):
     '''
     Task 5
@@ -15,4 +16,29 @@ def nms(pred, score, threshold):
         s_f (M,7) 3D bounding boxes after NMS
         c_f (M,1) corresopnding confidence scores
     '''
-    pass
+    pred_bev = pred.copy()
+    pred_bev[:,1] =0 # set height to zero
+    pred_bev[:,3] = 1
+    s_f = []
+    c_f = []
+    #print(pred_bev)
+    # threshold = threshold + 0.0926
+    # print(threshold)
+    while pred.shape[0] > 0:
+        print("pred shape",pred.shape[0])
+        idx_max_score = np.argmax(score)
+        s_f.append(pred[idx_max_score,:])
+        c_f.append(score[idx_max_score])
+        IoU = get_iou(pred_bev,pred_bev)
+        flag = IoU[idx_max_score,:] < threshold
+        #print(np.min(IoU))
+        indices = np.flatnonzero(flag)
+        print(indices)
+        pred = pred[indices,:]
+        pred_bev = pred_bev[indices,:]
+        score = score[indices]
+    s_f = np.array(s_f)
+    c_f = np.array(c_f)
+    print("real",s_f)
+    return s_f, c_f
+    
