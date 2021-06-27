9031f585bde2edae99cf77abb7c57329404ec6b6
nhoischen <nhoischen@student.ethz.ch>
2021-06-27 14:07:04 +0000

    Replace vis.py

--- utils/vis.py
+++ utils/vis.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 import wandb
-
+import torch
 from utils.task1 import get_iou, label2corners
 
 def point_scene(points, pred, target, threshold=0.5, name='test'):
@@ -27,6 +27,29 @@ def point_scene(points, pred, target, threshold=0.5, name='test'):
                           'label': '',
                           'color': [255,255,255]})
 
+    return {name: wandb.Object3D({
+                'type': 'lidar/beta',
+                'points': points,
+                'boxes': np.array(all_boxes)
+           })}
+
+def visualizeTask(points, pred, name='Task_visualization'):
+    '''
+    points (N,3) point cloud
+    pred (N,7) predicted bounding boxes (N,1) scores
+    target (N,7) target bounding boxes
+    threshold (float) when to consider a prediction correct
+    '''
+    all_boxes = []
+    if torch.is_tensor(pred):
+        pred = pred.detach().cpu().numpy()
+    if torch.is_tensor(points):
+        points = points.detach().cpu().numpy()
+    for i, p in enumerate(label2corners(pred)):
+        all_boxes.append({'corners': p.tolist(),
+                          'label': f'{pred[i,:]}',
+                          'color': [0,255,0]})
+    
     return {name: wandb.Object3D({
                 'type': 'lidar/beta',
                 'points': points,
