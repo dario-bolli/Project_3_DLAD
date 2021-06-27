b683fd66026181f3803126bef0c9598dccd6e5d0
dbolli <dbolli@student.ethz.ch>
2021-06-27 09:26:38 +0000

    Replace task4.py

--- utils/task4.py
+++ utils/task4.py
@@ -32,11 +32,17 @@ class RegressionLoss(nn.Module):
             #positive samples
             if iou[i] >= self.config['positive_reg_lb']:  #self.config['positive_reg_lb'] instead of 0.55
                 index = np.append(index,int(i))
+<<<<<<< HEAD
+        if len(index) == 0:
+            loss = torch.zeros(1)
+        else:
+=======
         if index.size == 0:
             loss = 0
         else:
             #index = [int(i) for i in index]     #convert indices to int (only type accepted in delete), but why is it not int at first?
             
+>>>>>>> 8376a4dee244fc5c30cf59144522da220e6b1749
             filtered_pred= pred[index].to(device) #np.delete(pred,index,axis=0)
             filtered_target = target[index].to(device) #np.delete(target,index,axis=0)    
             
@@ -83,6 +89,21 @@ class ClassificationLoss(nn.Module):
             if iou[i] <= self.config['negative_cls_ub']:
                 index_n = np.append(index_n,int(i))
 
+<<<<<<< HEAD
+        if len(index_p) == 0 and len(index_n) == 0:
+            loss = torch.zeros(1)
+        else:
+            filtered_pred_p= pred[index_p].to(device) 
+            filtered_pred_n= pred[index_n].to(device) 
+
+            label_n = np.zeros(filtered_pred_n.shape[0], dtype = np.float32).reshape(-1,1)
+            label_p = np.ones(filtered_pred_p.shape[0], dtype = np.float32).reshape(-1,1)
+            
+            label = np.vstack((label_n,label_p))
+            filtered_pred = torch.vstack((filtered_pred_n,filtered_pred_p)).to(device)
+            
+            loss = self.loss(filtered_pred, torch.from_numpy(label).to(device))
+=======
         #index_p = [int(i) for i in index_p]     #convert indices to int (only type accepted in delete)
         #index_n = [int(i) for i in index_n]
 
@@ -96,4 +117,5 @@ class ClassificationLoss(nn.Module):
         filtered_pred = torch.vstack((filtered_pred_n,filtered_pred_p)).to(device)
         
         loss = self.loss(filtered_pred, torch.from_numpy(label).to(device))
+>>>>>>> 8376a4dee244fc5c30cf59144522da220e6b1749
         return loss.to(device)
